298b12e98f749b3c081a2f4d5a062b7229fe50ea
Seungho Bak <seunghobak@yahoo.com>
2018-12-28 20:48:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(n) {
+    return document.querySelector(n)
+  }
 
+
+
+function nestedTarget() {
+  return  document.querySelector('div#nested div.target')
+}
+
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list');
+  for (let i = 0; i < lis.length; i++) {
+    parseInt(lis[i].innerHTML = (i + n).toString(), 10);
+  }
+}
+
+function deepestChild() {
+return document.querySelector('div#grand-node div div div div')
+}
